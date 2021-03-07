9b1fcc5211d09ad1ee37c206dd1db16c5ce332de
Viktor <69218609+PussyCartel@users.noreply.github.com>
2021-03-07 15:45:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests as rq
+import bs4
+import wget
+
+
+def parse_page(text):
+    soup = bs4.BeautifulSoup(text, 'html.parser')
+    arr = [link.get('href') for link in soup.find_all('a', 'uk-link-toggle')]
+    return arr
+
+
+def get_web_page(url):
+    r = rq.get(url)
+    text_page = r.text
+    return text_page
+
+
+if __name__ == '__main__':
+    text = get_web_page("https://www.mirea.ru/schedule/")
+    array_of_urls = parse_page(text)[:49]
+    for url in array_of_urls:
+        wget.download(url, out="files/")
---
+++ xlsxParser.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+import math
+
+
+def get_groups(line):
+    table = pd.read_excel(line, header=1)
+    S = pd.Series(table.columns.tolist())
+    groups = [key for i, key in enumerate(S.str.findall('....-[0-9][0-9]-[0-9][0-9]'))]
+    groups = pd.Series(groups).dropna()
+    groups = [key[0] for key in groups if len(key) == 1]
+    return groups
+
+
+def get_dataframe(line, length, groups):
+    table = pd.read_excel(line, header=2, nrows=72)
+    cols_empty = 0
+    cols_empty_realy_empty= 0
+    for i in range(length+3):
+        columns = ['Предмет', 'Вид\nзанятий', 'ФИО преподавателя', '№ \nауд.']
+        if i >= 1:
+            columns = [f'Предмет.{cols_empty-cols_empty_realy_empty}', 'Вид\nзанятий', 'ФИО преподавателя', '№ \nауд.']
+            columns[1:4] = list(map(lambda x: x+f".{cols_empty}", columns[1:4]))
+            if table[columns[1]].isnull().sum()>70:
+                cols_empty_realy_empty += 1
+                cols_empty += 1
+        pd.DataFrame({'idAndDayOfWeek': [i // 12 + 1 for i in range(6 * 12)],
+                      "idAndNumOfLesson": [math.floor((i % 12) * 0.5) + 1 for i in range(6 * 12)],
+                      'idAndParityOfWeek': [i % 2 + 1 for i in range(6 * 12)],
+                      'idAndTitleOfLessons': table[columns[0]],
+                      'idAndTypeOfLesson': table[columns[1]],
+                      'idAndTeacher': table[columns[2]],
+                      'idAndRoom': table[columns[3]]
+                      }).to_json(f'json/{groups[i]}.json')
+        cols_empty += 1
+
+
+for link in os.listdir("files/"):
+    group = get_groups('files/'+link)
+    try:
+        get_dataframe('files/'+link, len(group), group)
+    except:
+        pass
